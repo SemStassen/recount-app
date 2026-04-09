6e1e80d55f6b43df9935867cf17fc4cfed6091e3
Sem Stassen <semstassen@gmail.com>
2026-04-09 13:31:51 +0200

    Simplify auth atoms

--- interface/src/atoms/auth.atoms.ts
+++ interface/src/atoms/auth.atoms.ts
@@ -1,26 +1,17 @@
-import { Effect } from "effect";
-import { Atom } from "effect/unstable/reactivity";
-
 import { RecountAtomRpcClient } from "~/lib/rpc/atom-client";
 
-import { atomRuntime } from "./runtime";
+export const sessionAtom = RecountAtomRpcClient.query(
+  "Auth.GetSession",
+  undefined,
+  {
+    timeToLive: Infinity,
+  }
+);
 
-export const sessionAtom = atomRuntime
-  .atom(
-    Effect.gen(function* () {
-      const client = yield* RecountAtomRpcClient;
-
-      return yield* client("Auth.GetSession", undefined);
-    })
-  )
-  .pipe(Atom.keepAlive);
-
-export const workspacesAtom = atomRuntime
-  .atom(
-    Effect.gen(function* () {
-      const client = yield* RecountAtomRpcClient;
-
-      return yield* client("Workspace.List", undefined);
-    })
-  )
-  .pipe(Atom.keepAlive);
+export const workspacesAtom = RecountAtomRpcClient.query(
+  "Workspace.List",
+  undefined,
+  {
+    timeToLive: Infinity,
+  }
+);
